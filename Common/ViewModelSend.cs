822f9c2976f63a3a9d3be19ef7c86f9882cb0aaa
Milosh <mutagarova05@inbox.ru>
2024-11-23 21:52:01 +0500

    Созданный класс ViewModelSend. Mutagarova

---
+++ Common/ViewModelSend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelSend
+    {
+        public string Message { get; set; }
+        public int Id { get; set; }
+
+        public ViewModelSend(string Message, int Id)
+        {
+            this.Message = Message;
+            this.Id = Id;
+        }
+    }
+
+}
