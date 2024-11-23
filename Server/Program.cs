b7fa0a64c4ba6fcdd9a9cca9b395140d4a5dc068
Milosh <mutagarova05@inbox.ru>
2024-11-23 21:59:31 +0500

    Добавленные переменные сервера. Мутагарова

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAdress;
+        public static int Port;
+
         static void Main(string[] args)
         {
         }
